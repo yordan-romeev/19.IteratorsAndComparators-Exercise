ed3f7a616c9f2b6d35a378e7451c64513fe7e8c6
Yordan Romeev <yordan.romeev@gmail.com>
2024-12-07 15:02:29 +0200

    Add Book Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Book book1 = new Book("Title1", 1990, "Author1", "Author 2");
+        Book book2 = new Book("Title2", 1980, "Author3");
+        Book book3 = new Book("Title3", 1970);
+
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+
+
     }
 }
